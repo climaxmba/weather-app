a8c40a1b422eb3bf0eb8c8d50e3dfdecab0dc991
Climax Mba <climaxium150@gmail.com>
2023-08-23 11:03:30 +0100

    Add pubSub.js & handleAPI.js

---
+++ src/modules/handleAPI.js
@@ -0,0 +1,24 @@
+async function getForecastData(location) {
+    const key = "c52eaecafe624ab6908202749232108";
+    // Geocode https://geocode.maps.co/search?q=
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=3`;
+    let result, data;
+    try {
+        result = await fetch(url, { mode: "cors" });
+        if (result.ok) {
+          data = await result.json();
+          return data;
+        } else {
+            throw new Error(result.status);
+        }
+    } catch (err) {
+        return Promise.reject(err);
+    }
+}
+
+async function getSpecificData(selection, location) {
+    const result = await getForecastData(location);
+    selection ? console.log(result[selection]) : console.log(result);
+}
+
+export default getSpecificData;
---
+++ src/modules/pubSub.js
@@ -0,0 +1,19 @@
+const pubSub = (function () {
+    let events = {};
+  
+    function subscribe(event, fn) {
+      events[event] ? events[event].push(fn) : (events[event] = [fn]);
+    }
+    function unSubscribe(event, fn) {
+      if (events[event]) {
+        events[event] = events[event].filter((func) => func !== fn);
+      }
+    }
+    function publish(event, data) {
+      if (events[event]) events[event].forEach((fn) => fn(data));
+    }
+  
+    return { subscribe, unSubscribe, publish };
+  })();
+
+  export default pubSub;
