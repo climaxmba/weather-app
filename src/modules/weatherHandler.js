ac388cc11b530b0b9d8d55c8902d7d3844a9035a
Climax Mba <climaxium150@gmail.com>
2023-08-24 17:48:54 +0100

    Add ip look-up & improve error handling

--- src/modules/weatherHandler.js
+++ src/modules/weatherHandler.js
@@ -3,10 +3,25 @@ import events from "./pubSubEvents.js";
 import ft from "format-time";
 
 const weather = (() => {
-    pubSub.subscribe(events.dataSearched, _searchData);
-    
+  pubSub.subscribe(events.dataSearched, _searchData);
+
   async function init() {
-    _searchData("Moscow");
+    _getUserCoord()
+      .then((ipData) => _searchData(`${ipData.latitude},${ipData.longitude}`))
+      .catch((err) => {
+        console.log(err);
+        _searchData("Texas");
+      });
+  }
+
+  async function _getUserCoord() {
+    try {
+      const response = await fetch("https://freeipapi.com/api/json");
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      return Promise.reject("FAILED TO GET USER IP COORDINATES\n");
+    }
   }
 
   async function _getCurrentData(location) {
@@ -28,36 +43,48 @@ const weather = (() => {
   }
 
   async function _getParsedData(location) {
-    const result = await _getCurrentData(location);
-    const currentData = result.current,
-      locationData = result.location,
-      emptyContent = "_ _ _ ";
+    try {
+      const result = await _getCurrentData(location);
+      const currentData = result.current,
+        locationData = result.location,
+        emptyContent = "_ _ _ ";
 
-    return {
-      city: locationData.name || emptyContent,
-      country: `${locationData.region || emptyContent}, ${locationData.country || emptyContent}`,
-      time: ft.getFormattedTime(locationData.localtime.split(" ")[1]),
-      date: new Date(locationData.localtime.split(" ")[0]).toLocaleDateString(),
-      metricTemp: `${currentData.temp_c} ℃`,
-      imperialTemp: `${currentData.temp_f} ℉`,
-      icon: currentData.condition.icon,
-      conditionText: currentData.condition.text,
-      metricWind: `${currentData.wind_kph} kph, ${currentData.wind_dir}`,
-      imperialWind: `${currentData.wind_mph} mph, ${currentData.wind_dir}`,
-      metricPressure: `${currentData.pressure_mb} mb`,
-      imperialPressure: `${currentData.pressure_in} in`,
-      humidity: currentData.humidity,
-      metricVisibility: `${currentData.vis_km} km`,
-      imperialVisibility: `${currentData.vis_miles} miles`,
-      uv: currentData.uv,
-      metricGust: `${currentData.gust_kph} kph`,
-      imperialGust: `${currentData.gust_mph} mph`,
-    };
+      return {
+        city: locationData.name || emptyContent,
+        country: `${locationData.region || emptyContent}, ${
+          locationData.country || emptyContent
+        }`,
+        time: ft.getFormattedTime(locationData.localtime.split(" ")[1]),
+        date: new Date(
+          locationData.localtime.split(" ")[0]
+        ).toLocaleDateString(),
+        metricTemp: `${currentData.temp_c} ℃`,
+        imperialTemp: `${currentData.temp_f} ℉`,
+        icon: currentData.condition.icon,
+        conditionText: currentData.condition.text,
+        metricWind: `${currentData.wind_kph} kph, ${currentData.wind_dir}`,
+        imperialWind: `${currentData.wind_mph} mph, ${currentData.wind_dir}`,
+        metricPressure: `${currentData.pressure_mb} mb`,
+        imperialPressure: `${currentData.pressure_in} in`,
+        humidity: currentData.humidity,
+        metricVisibility: `${currentData.vis_km} km`,
+        imperialVisibility: `${currentData.vis_miles} miles`,
+        uv: currentData.uv,
+        metricGust: `${currentData.gust_kph} kph`,
+        imperialGust: `${currentData.gust_mph} mph`,
+      };
+    } catch {
+      return Promise.reject("FAILED TO PARSE DATA\n");
+    }
   }
 
   async function _searchData(query) {
-    const data = await _getParsedData(query);
-    pubSub.publish(events.dataRecieved, data);
+    try {
+      const data = await _getParsedData(query);
+      pubSub.publish(events.dataRecieved, data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return { init };
