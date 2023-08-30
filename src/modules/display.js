b7c83825b8c27db9bfba4c6ae96975644c3ccf72
Climax Mba <climaxium150@gmail.com>
2023-08-30 06:12:27 +0100

    Modify autocomplete mechanism

--- src/modules/display.js
+++ src/modules/display.js
@@ -61,7 +61,7 @@ const displayController = (() => {
 
     for (let i = 0; i < list.length; i++) {
       const node = document.createElement("li");
-      node.setAttribute("data-coord", `${list[i].lat}, ${list[i].lon}`);
+      node.setAttribute("data-addr", `${list[i].display_name}`);
       node.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg>
         <span class="area-name">${list[i].display_name}</span>
         <div class="category">
@@ -88,8 +88,8 @@ const displayController = (() => {
     if (e.target !== e.currentTarget) {
       _removeAreasList();
       let elem = e.target;
-      while (!elem.hasAttribute("data-coord")) elem = elem.parentElement;
-      _searchData(elem.getAttribute("data-coord"));
+      while (!elem.hasAttribute("data-addr")) elem = elem.parentElement;
+      _searchData(elem.getAttribute("data-addr"));
     }
   }
   function _searchData(addr) {
