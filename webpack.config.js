6504b5ff553370c818e9a8c4a67ce182880982ae
Climax Mba <climaxium150@gmail.com>
2023-08-31 17:05:00 +0100

    Remove obsolete config

--- webpack.config.js
+++ webpack.config.js
@@ -8,20 +8,4 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-        ],
-    },
 };
